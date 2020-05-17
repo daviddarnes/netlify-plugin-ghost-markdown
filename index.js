d197a6eb62d965028722d892889e9f67d1b1c684
David Darnes <me@daviddarnes.com>
2020-05-17 18:47:44 +0100

    Get image paths working properly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const fetch = require("node-fetch");
 const url = require("url");
-const chalk = require('chalk');
+const chalk = require("chalk");
 const ghostContentAPI = require("@tryghost/content-api");
 
 // Get posts using Ghost Content API
@@ -34,14 +34,12 @@ const getPages = async (api, failPlugin) => {
 // Download images
 const downloadImage = async (inputURI, outputPath, failPlugin) => {
   try {
-
     // Grab file data from remote inputURI
     const res = await fetch(inputURI);
     const fileData = await res.buffer();
 
     // Write the file and cache it
     await fs.outputFile(outputPath, fileData);
-
   } catch (error) {
     failPlugin("Image file error", { error });
   }
@@ -52,19 +50,15 @@ const getRelativePath = (assetsDir, docDir) => {
   const explodedAssetsDir = assetsDir.split("/");
   const explodedDocDir = docDir.split("/");
 
-  console.log(explodedAssetsDir);
-  console.log(explodedDocDir);
-
-  explodedAssetsDir.map((slice, index) => {
-    console.log("asset slice " + slice)
-    console.log("doc slice   " + explodedDocDir[index]);
-    console.log("matches     " + slice == explodedDocDir[index]);
+  const difference = explodedAssetsDir.findIndex((slice, index) => {
+    return explodedDocDir[index] != slice;
   });
+
+  return "/" + explodedAssetsDir.slice(difference).join("/");
 };
 
 // Markdown template
 const mdTemplate = (item, imagePath, assetsDir, layout) => {
-
   // Format fearture image path
   const formatFeatureImage = (path) => {
     if (path) {
@@ -130,7 +124,6 @@ module.exports = {
       cache
     }
   }) => {
-
     // Ghost images path
     const ghostImagePath = ghostURL + "/content/images/";
 
@@ -149,7 +142,6 @@ module.exports = {
 
     // Find all images
     const findImages = (allContent) => {
-
       // Find all posts and pages with images in the HTML
       const htmlWithImages = allContent
         .filter((item) => item.html && item.html.includes(ghostImagePath))
@@ -180,60 +172,65 @@ module.exports = {
 
     // Generate all images, posts and pages…
     await Promise.all([
-
       // Replace Ghost image paths with local ones
-      ...findImages([...posts, ...pages]).map( async (image) => {
-
+      ...findImages([...posts, ...pages]).map(async (image) => {
         // Image destination
         const dest = image.replace(ghostImagePath, assetsDir);
 
         // Check if image is in Netlify cache
         if (await cache.has(dest)) {
-
           // Restore image from cache
           await cache.restore(dest);
-          console.log(chalk.green('Restored from cache: ') + chalk.green.underline(dest));
-
+          console.log(
+            chalk.green("Restored from cache: ") + chalk.green.underline(dest)
+          );
         } else {
-
           // …otherwise download the image and cache it
           await downloadImage(image, dest, failPlugin);
           await cache.save(dest);
-          console.log(chalk.cyan('Downloading and caching: ') + chalk.cyan.underline(dest));
+          console.log(
+            chalk.cyan("Downloading and caching: ") + chalk.cyan.underline(dest)
+          );
         }
       }),
 
       // Generate markdown posts
       ...posts.map(async (post) => {
-
         // Prefix filename with date, Jekyll style formatting
         const filename = postDatePrefix
           ? `${post.published_at.slice(0, 10)}-${post.slug}`
           : post.slug;
 
-        console.log("index " + getRelativePath(assetsDir, postsDir));
+        // Remove initial matching paths for relative source path
+        const relativeAssetsDir = getRelativePath(assetsDir, postsDir);
 
         // Create post markdown file with content
         await writeMarkdown(
           postsDir,
           `${filename}.md`,
-          mdTemplate(post, ghostImagePath, assetsDir, postsLayout),
+          mdTemplate(post, ghostImagePath, relativeAssetsDir, postsLayout),
           failPlugin
         );
-        console.log(chalk.gray('Generated post: ') + chalk.gray.underline(post.title));
+        console.log(
+          chalk.gray("Generated post: ") + chalk.gray.underline(post.title)
+        );
       }),
 
       // Generate markdown pages
       ...pages.map(async (page) => {
+        // Remove initial matching paths for relative source path
+        const relativeAssetsDir = getRelativePath(assetsDir, pagesDir);
 
         // Create page markdown file with content
         await writeMarkdown(
           pagesDir,
           `${page.slug}.md`,
-          mdTemplate(page, ghostImagePath, assetsDir, pagesLayout),
+          mdTemplate(page, ghostImagePath, relativeAssetsDir, pagesLayout),
           failPlugin
         );
-        console.log(chalk.gray('Generated page: ') + chalk.gray.underline(page.title));
+        console.log(
+          chalk.gray("Generated page: ") + chalk.gray.underline(page.title)
+        );
       })
     ]);
   }
